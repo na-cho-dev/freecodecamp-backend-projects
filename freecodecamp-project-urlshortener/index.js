b265283d53896b6f5d3ce92b0fed8f49b993ccd8
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-19 22:39:15 +0100

    Update: URL Shortner Project

---
+++ freecodecamp-project-urlshortener/index.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const dns = require('dns');
+const crypto = require('crypto');
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+const urlDatabase = {};
+
+app.post('/api/shorturl', (req, res) => {
+  originalUrl = req.body.url;
+
+  if (!originalUrl) {
+    return res.status(400).json({ error: 'Invalid URL' });
+  }
+
+  try {
+    const validUrl = new URL(originalUrl);
+
+    dns.lookup(validUrl.hostname, (err) => {
+      if (err) {
+        return res.status(400).json({ error: 'Invalid Hostname' });
+      } else {
+        const hashedUrl = crypto
+          .createHash('md5')
+          .update(validUrl.href)
+          .digest('hex')
+          .slice(0, 6);
+        urlDatabase[hashedUrl] = originalUrl;
+        console.log(hashedUrl);
+        return res.json({
+          original_url: originalUrl,
+          short_url: hashedUrl,
+        });
+      }
+    });
+  } catch (err) {
+    return res.status(400).json({ error: 'invalid url' });
+  }
+});
+
+app.get('/api/shorturl/:urlNumber', (req, res) => {
+  const urlNumber = req.params.urlNumber;
+  const originalUrl = urlDatabase[urlNumber];
+
+  if (!originalUrl)
+    return res
+      .status(404)
+      .json({ error: 'No short URL found for the given input' });
+
+  res.redirect(originalUrl);
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
